1bb7859edc7fda69c838e5ad1e0bd4298632d8e8
maxsibilla <maxsibilla@icloud.com>
2020-07-13 16:27:16 -0400

    Fixing line to run droolstest over date range

--- runRules.sh
+++ runRules.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [[ $(uname) -eq 'Darwin' ]]; then
+  date() { gdate "$@"; }
+fi
 
 if [ "$1" = "simulated" ]; then
   dates=(2008-02-13 2008-02-14) ## simulated data public schema dates
@@ -24,7 +27,7 @@ elif [ "$1" = "custom" ]; then
 
   startdate=$(date -I -d "$2") || echo "ERROR: Must enter start date followed by end date (e.g. custom 2020-01-01 2020-02-01)" exit 1
   enddate=$(date -I -d "$3") || echo "ERROR: Must enter start date followed by end date (e.g. custom 2020-01-01 2020-02-01)" exit 1
-
+  enddate=$(date -I -d "$enddate + 1 day")
   if [[ "$enddate" < "$startdate" ]]; then
     echo "ERROR: Must enter start date followed by end date (e.g. custom 2020-01-01 2020-02-01)"
     exit 1
@@ -34,8 +37,8 @@ elif [ "$1" = "custom" ]; then
   while [ "$d" != "$enddate" ]; do
     echo INFO: Running engine on data extracted for $d
     echo INFO: Output for this run will be written to $1-run/output-$d/idia-rules-run-$d.txt
-    mkdir $1-run/output-$d
-    java -Xmx8192m -jar target/droolstest-1.0.jar "$d" ${@:2} >$1-run/output-"$d"/idia-rules-run-"$d".txt
+    mkdir $1-run/output-"$d"
+    java -Xmx8192m -jar target/droolstest-1.0.jar "$d" >$1-run/output-"$d"/idia-rules-run-"$d".txt
     sleep 1
     d=$(date -I -d "$d + 1 day")
   done
@@ -45,7 +48,7 @@ else
 fi
 
 echo INFO: run completed. Assembling the resulting data and writing it to $1-run/full-$1-run.tsv
-grep -r "^DATA" $1-run/output* | sort | cut --complement -f1 >$1-run/full-$1-run.tsv
+grep -r "^DATA" $1-run/output* | sort | gcut --complement -f1 >$1-run/full-$1-run.tsv
 echo INFO: Assembling the unique resulting data and writing it to $1-run/uniq-$1-run.tsv
-grep -r "^DATA" $1-run/output* | sort | uniq | cut --complement -f1 >$1-run/uniq-$1-run.tsv
+grep -r "^DATA" $1-run/output* | sort | uniq | gcut --complement -f1 >$1-run/uniq-$1-run.tsv
 echo INFO: All done!
